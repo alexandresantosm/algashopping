d2c6cd7e3c96014a47d83a1fab5b354693a0de7c
Alexandre Santos <amenezes031@gmail.com>
2022-07-02 21:46:49 -0300

    :sparkles: feat: create App component

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { App } from "../components/App";
 
 export const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>todo: home page</h1>
+      <App />
     </div>
   );
 };
