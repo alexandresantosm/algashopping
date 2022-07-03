44a0c144f60d113fd4ea5f3fe3d6ecdd230920cb
Alexandre Santos <amenezes031@gmail.com>
2022-07-03 18:44:14 -0300

    :lipstick: refactor: remove title styles

--- src/components/AppContainer/AppContainer.styles.ts
+++ src/components/AppContainer/AppContainer.styles.ts
@@ -1,18 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
+  height: calc(100% - 70px);
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-column-gap: 1.5rem;
 
   padding: 1.5rem;
-
-  section {
-    h2 {
-      height: 2.5rem;
-
-      font-size: 1rem;
-      font-weight: normal;
-    }
-  }
 `;
