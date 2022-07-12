26f86a1307c1deecda8a0ee83237a192f91e9648
Alexandre Santos <amenezes031@gmail.com>
2022-07-12 19:05:31 -0300

    :sparkles: feat: add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
+import { Provider } from 'react-redux';
+
 import { HomePage } from "./pages/Home.page";
+import { store } from './store'
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <Fragment>
-      <HomePage />
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
 
       <GlobalStyle />
     </Fragment>
