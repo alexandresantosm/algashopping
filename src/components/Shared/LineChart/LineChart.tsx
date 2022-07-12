c0579764a778bcdab58ccc3754fdd687a9452dd5
Alexandre Santos <amenezes031@gmail.com>
2022-07-12 19:15:05 -0300

    :recycle: refactor: get selected products from global state

--- src/components/Shared/LineChart/LineChart.tsx
+++ src/components/Shared/LineChart/LineChart.tsx
@@ -1,18 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import { selectSelectedProducts } from "../../../store/Products/Products.selectors";
+import { extractPercentage } from "../../../utils/extractPercentage";
 
 import { ProgressBar, Wrapper } from "./LineChart.styles";
 
 interface LineChartProps {
   title: string;
-  percentage?: number;
+  tag: string;
   progressColor?: string;
 }
 
 export const LineChart: React.FC<LineChartProps> = ({
-  percentage,
   progressColor,
   title,
+  tag
 }) => {
+  const selectedProducts = useSelector(selectSelectedProducts);
+  
+  const percentage = extractPercentage(
+    selectedProducts.length,
+    selectedProducts.filter((product) =>
+      product.tags.includes(tag)
+    ).length
+  );
+
   return (
     <Wrapper>
       <span>{title}</span>
